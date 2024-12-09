b773fb7458e49fb59a4eb070b22f8815c168f5fd
Niki-Nicoli <twwr.inc@gmail.com>
2024-12-08 20:45:56 -0500

    5 day forecast update

--- JavaScript.js
+++ JavaScript.js
@@ -21,26 +21,79 @@ function cityWeather(response) {
   Humidity.innerHTML = `${response.data.temperature.humidity}%`;
   let windSpeed = document.querySelector(".wind");
   windSpeed.innerHTML = `${response.data.wind.speed}km/hr`;
+  let icon = document.querySelector(".emoji");
+  icon.innerHTML = `<img src= "${response.data.condition.icon_url}"/>`;
   let current = document.querySelector(".date");
   let currentDate = new Date(response.data.time * 1000);
   console.log(currentDate);
+  currentDate.innerHTML = getDate(currentDate);
+  getForecastData(response.data.city);
+}
+
+function getDate(currentDate) {
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
   let days = [
     "Sunday",
     "Monday",
     "Tuesday",
-    "Wendesday",
+    "Wednesday",
     "Thursday",
-    "Frdayi",
+    "Friday",
     "Saturday",
   ];
   let Day = days[currentDate.getDay()];
-  console.log(`${Day}, ${currentDate.getHours()}:${currentDate.getMinutes()}`);
-  current.innerHTML = `${Day}, ${currentDate.getHours()}:${currentDate.getMinutes()}`;
-  let icon = document.querySelector(".emoji");
-  icon.innerHTML = `<img src= "${response.data.condition.icon_url}"/>`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  console.log(`${Day}, ${hours}:${minutes}`);
+  return `${Day}, ${hours}:${minutes}`;
+}
+
+function getForecastData(city) {
+  let forecastURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=5452ccec034a0afbo38d92e109tf8e74&units=metric`;
+  console.log(forecastURL);
+  axios.get(forecastURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let weatherForecastHTML = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      weatherForecastHTML =
+        weatherForecastHTML +
+        `<div class="weather-forecast-day"><div class="weekDay">${getForcastDate(
+          day.time
+        )}</div><div class="forecast-emoji"><img src= "${
+          day.condition.icon_url
+        }"/> </div><div class="forecast-temperatures"> ${Math.round(
+          day.temperature.maximum
+        )}&deg<span class="feel-temp ">${Math.round(
+          day.temperature.minimum
+        )}&deg;</span> </div></div>`;
+    }
+  });
+  let weatherDisplay = document.querySelector("#weather-forecast");
+  weatherDisplay.innerHTML = weatherForecastHTML;
+}
+
+function getForcastDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[date.getDay()];
 }
 
 let citySearch = document.querySelector("#citySearch");
 citySearch.addEventListener("submit", cityName);
 
 cityNameSearch("Bronx");
+displayForecast();
